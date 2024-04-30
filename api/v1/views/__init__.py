3375949f6dcb7c968ceeba9d0c0ffdee4c88ec59
Desheye <aarealaafiaboyz@gmail.com>
2024-04-30 08:52:05 +0100

    right order

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,12 +3,13 @@
 the views
 """
 
-from api.v1.views.states import *
-from api.v1.views.cities import *
-from api.v1.views.index import *
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views import states
+from api.v1.views import cities
+from api.v1.views import index
 
 # Import and expose specific modules
 
